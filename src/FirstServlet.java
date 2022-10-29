2a66ff32c40eaa67200db56e8b13a5560479bfed
Alex Bolodurin <shur026@yandex.ru>
2022-10-29 18:59:01 +0300

    1.0 First Servlet

---
+++ src/FirstServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html>");
+        out.println("<h1> Fuck You! </h1>");
+        out.println("</html>");
+
+    }
+}
